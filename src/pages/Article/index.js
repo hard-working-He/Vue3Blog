6af2c71707e8d30d62708698b529f3dac17ce74b
lipstickof87 <2380588373@qq.com>
2023-11-08 19:38:04 +0800

    finish 4.1~4.3

--- src/pages/Article/index.js
+++ src/pages/Article/index.js
@@ -1,4 +1,127 @@
+import { Link } from 'react-router-dom'
+import { Card, Breadcrumb, Form, Button, Radio, DatePicker, Select } from 'antd'
+//import 'moment/locale/zh-cn'
+//import locale from 'antd/es/date-picker/locale/zh_CN'
+import './index.scss'
+import { http } from '../../utils'
+import { Table, Tag, Space } from 'antd'
+import { EditOutlined, DeleteOutlined } from '@ant-design/icons'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+const { Option } = Select
+const { RangePicker } = DatePicker
+
 const Article = () => {
-  return <div>Article</div>
+  const columns = [
+    {
+      title: '封面',
+      dataIndex: 'cover',
+      width:120,
+      render: cover => {
+        return <img src={cover } width={80} height={60} alt="" />
+      }
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      width: 220
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      render: data => <Tag color="green">审核通过</Tag>
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'pubdate'
+    },
+    {
+      title: '阅读数',
+      dataIndex: 'read_count'
+    },
+    {
+      title: '评论数',
+      dataIndex: 'comment_count'
+    },
+    {
+      title: '点赞数',
+      dataIndex: 'like_count'
+    },
+    {
+      title: '操作',
+      render: data => {
+        return (
+          <Space size="middle">
+            <Button type="primary" shape="circle" icon={<EditOutlined />} />
+            <Button
+              type="primary"
+              danger
+              shape="circle"
+              icon={<DeleteOutlined />}
+            />
+          </Space>
+        )
+      }
+    }
+  ]
+
+  const data = [
+      {
+          id: '8218',
+          comment_count: 0,
+          cover: {
+            images:['http://geek.itheima.net/resources/images/15.jpg'],
+          },
+          like_count: 0,
+          pubdate: '2019-03-11 09:00:00',
+          read_count: 2,
+          status: 2,
+          title: 'wkwebview离线化加载h5资源解决方案' 
+      }
+  ]
+  const [channels, setChannels] = useState([])
+  useEffect(
+    () => {
+      async function fetchChannels () {
+        const res = await http.get('./channels')
+       
+        setChannels(res.data.data.channels)
+      }
+      fetchChannels()
+    },[])
+
+  return (
+    <div>
+      <Card
+        title={
+          <Breadcrumb separator=">">
+            <Breadcrumb.Item>
+              <Link to="/home">首页</Link>
+            </Breadcrumb.Item>
+            <Breadcrumb.Item>内容管理</Breadcrumb.Item>
+          </Breadcrumb>
+        }
+        style={{ marginBottom: 20 }}
+      >
+        <Form initialValues={{ status: null }}>
+        <Form.Item label="频道" name="channel_id" >
+          <Select placeholder="请选择文章频道" style={{ width: 200 }} >
+          {channels.map(item => (
+            <Option key={item.id} value={item.id}>
+            {item.name}
+            </Option>
+      ))}
+          </Select>
+</Form.Item>
+        </Form>
+      </Card>
+      
+      <Card title={`根据筛选条件共查询到 count 条结果：`}>
+        <Table rowKey="id" columns={columns} dataSource={data} />
+      </Card>
+    </div>
+  )
 }
+
 export default Article
